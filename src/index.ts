ced63bfe8dcfdbc6d424caf500ae08b2bc224de7
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-12-04 19:15:43 -0300

    feat: docs dir and class diagram added.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import users from './routes/users';
 
 const PORT = 3000;
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(users);
+app.use(cors({origin: '*'}));
 
 app.get('/', (req, res) => { res.send("API is running!"); });
 
